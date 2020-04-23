e26da09885c0a8b617be0c3d3648457e4196a5eb
Romain Zanon <romain.zanon@canal-plus.com>
2020-04-23 11:55:59 +0200

    init project

---
+++ app/src/main/java/com/canal/android/test/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.canal.android.test.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.canal.android.test.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
